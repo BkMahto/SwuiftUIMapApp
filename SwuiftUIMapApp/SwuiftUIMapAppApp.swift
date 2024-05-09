0964354196c9a3505a61b09b9492dc3695581944
Bandan Kumar Mahto <159515893+BkMahto@users.noreply.github.com>
2024-05-09 09:39:24 +0530

    Initial Commit

---
+++ SwuiftUIMapApp/SwuiftUIMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwuiftUIMapAppApp.swift
+//  SwuiftUIMapApp
+//
+//  Created by elluminati on 09/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SwuiftUIMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
