4e0d025d0cc83dc5bcd52630f93384f16138fdc5
Bandan Kumar Mahto <159515893+BkMahto@users.noreply.github.com>
2024-05-09 10:00:20 +0530

    Model Created

---
+++ SwuiftUIMapApp/Model/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  SwuiftUIMapApp
+//
+//  Created by elluminati on 09/05/24.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
