9cbef52c19b63b176d26d582540c2e58c725ccb8
Bandan Kumar Mahto <159515893+BkMahto@users.noreply.github.com>
2024-05-09 11:38:32 +0530

    Traversing Login Done

--- SwuiftUIMapApp/ViewModel/LocationViewModel.swift
+++ SwuiftUIMapApp/ViewModel/LocationViewModel.swift
@@ -5,13 +5,75 @@
 //  Created by elluminati on 09/05/24.
 //
 
-import Foundation
+import SwiftUI
+import MapKit
 
 class LocationViewModel: ObservableObject {
+    
+    // All loaded location
     @Published var locations: [Location]
+    
+    // Current location on map
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
+    
+    // Show list of locations
+    @Published var showLocationsList: Bool = false
     
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first!
+        self.updateMapRegion(location: locations.first!)
     }
+    
+    private func updateMapRegion(location: Location) {
+        withAnimation(.easeInOut) {
+            mapRegion = MKCoordinateRegion(
+                center: location.coordinates,
+                span: mapSpan)
+        }
+    }
+    
+    func toggleLocationList() {
+        withAnimation(.easeInOut) {
+            showLocationsList = !showLocationsList            
+        }
+    }
+    
+    func showNextLocation(location: Location) {
+        withAnimation(.easeInOut) {
+            mapLocation = location
+            showLocationsList = false
+        }
+    }
+    
+    func nextButtonPressed() {
+        // Get current index
+//        let index = locations.firstIndex(where: { $0 == mapLocation })
+        guard let currentIndex = locations.firstIndex(of: mapLocation) else {
+            print("Could not find current index in locations array! Should never happen")
+            return
+        }
+        
+        let nextIndex = currentIndex + 1
+        guard locations.indices.contains(nextIndex) else {
+            // Next index is NOT VALID
+            // Restart from 0
+            guard let firstLocation = locations.first else { return }
+            showNextLocation(location: firstLocation)
+            return
+        }
+        
+        let nextLocation = locations[nextIndex]
+        showNextLocation(location: nextLocation)
+    }
+    
 }
